350156e0424350ee26083dd8e807b26e1593de07
srosenfeld <srosenfeld@users.noreply.github.com>
2017-08-22 13:07:38 -0400

    Taking away homedir from code,
    Replaced the homedir variable with the directory for Desktop to see if this fixes error

--- pyautogui_notepad_helloworld.py
+++ pyautogui_notepad_helloworld.py
@@ -32,8 +32,11 @@ except ImportError:
     print("Script proceeding - pyautogui now installed")
     import pyautogui as pg
 
+'''
+COMMENTED OUT FOR TESTING 8-22-17
 #Sets Home Directory for Current User
 homedir = os.environ['HOME']
+'''
 
 #Opens Task View Interface
 pg.keyDown('winleft')
@@ -82,7 +85,7 @@ pg.press('s')
 pg.keyUp('ctrl')
 
 #Names file "Hello world" + date and saves it to the desktop by closing "Save" window
-pg.typewrite((homedir + '\Desktop\\' + 'Hello world ' + str(time.strftime("%d-%m-%Y ")) + str(time.strftime("%H-%M-%S"))), interval = 0.01)
+pg.typewrite(( 'C:\\Users\\csci\\Desktop\\'+ 'Hello world ' + str(time.strftime("%d-%m-%Y ")) + str(time.strftime("%H-%M-%S"))), interval = 0.01)
 pg.press('enter')
 
 #Closes Notepad
